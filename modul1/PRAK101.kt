14976f6db98291f51b1fb52ed5296fe36a9c64ef
salsabilashofia <101611648+salsabilashofia@users.noreply.github.com>
2022-03-24 12:28:15 +0800

    Add files via upload

---
+++ modul1/PRAK101.kt
@@ -0,0 +1,13 @@
+fun main (args: Array<String>) {
+    print("Waktu Sekarang : ")
+    val waktu = readLine()
+    print("Nama Anda : ")
+    val nama = readLine()
+    print("Umur Anda : ")
+    val umur = readLine()
+    print("Suhu Tubuh Anda : ")
+    val suhu = readLine()
+
+    println("Selamat $waktu, $nama. \nUmur anda $umur tahun. \nSuhu Tubuh anda $suhu derajat Celcius")
+
+}
